a2d9aaffc6afb9109ce41dcc6dccf60bdaca6300
prototype2010 <alex.zakorko2015@gmail.com>
2020-06-10 09:03:30 -0800

    parsed document

--- init.rb
+++ init.rb
@@ -1,1 +1,15 @@
+require 'csv'
 
+rows_array = CSV.parse(File.read('athlete_events_small.csv'), headers: :first_row).map do |csvRow|
+  csv_row_hash = csvRow.to_hash
+
+  down_case_keys = Hash.new
+
+  csv_row_hash.each_key do |key|
+    down_case_keys[key.downcase] = csv_row_hash[key];
+  end
+
+  down_case_keys
+end
+
+rows_array

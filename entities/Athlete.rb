6951f8cc34756fed7612604730f0d580b6bc73ff
prototype2010 <alex.zakorko2015@gmail.com>
2020-07-01 09:42:17 +0300

    entity details

--- entities/Athlete.rb
+++ entities/Athlete.rb
@@ -2,19 +2,21 @@ require_relative '../db/model'
 
 class Athlete < Model
 
-  attr_writer @team_id
+  attr_accessor @team_id
 
   def initialize(csv_row_hash)
+    @id=csv_row_hash["id"]
     @full_name = csv_row_hash["full_name"]
     @sex = csv_row_hash["sex"]
     @year_of_birth = csv_row_hash["year_of_birth"]
     @params = Hash.new
+    @team_id = nil
     if csv_row_hash['height']
       @params["height"] = csv_row_hash['height']
     end
     if csv_row_hash['weight']
       @params["weight"] = csv_row_hash['weight']
     end
-    @team_id = nil
+
   end
 end
